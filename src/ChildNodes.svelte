37a5b4b9953fd69fe9734848fb8c6f388e91bd03
Russell McWhae <russell.mcwhae@gmail.com>
2020-10-26 13:20:58 -0700

    Removes trim()

--- src/ChildNodes.svelte
+++ src/ChildNodes.svelte
@@ -1,17 +1,17 @@
 <script>
-  import blockComponents from "./defaultBlocks";
-  export let childNodes = [];
-  export const assignProps = childNode =>
-    childNode.props && !!childNode.component ? childNode.props : {};
+	import blockComponents from './defaultBlocks'
+	export let childNodes = []
+	export const assignProps = childNode =>
+		childNode.props && !!childNode.component ? childNode.props : {}
 </script>
 
 {#each childNodes as childNode}
-  {#if typeof childNode === 'string'}{childNode.trim()}{/if}
-  {#if childNode.childNodes}
-    <svelte:component
-      this={childNode.component || blockComponents(childNode.tag)}
-      {...assignProps(childNode)}>
-      <svelte:self childNodes={childNode.childNodes} />
-    </svelte:component>
-  {/if}
+	{#if typeof childNode === 'string'}{childNode}{/if}
+	{#if childNode.childNodes}
+		<svelte:component
+			this={childNode.component || blockComponents(childNode.tag)}
+			{...assignProps(childNode)}>
+			<svelte:self childNodes={childNode.childNodes} />
+		</svelte:component>
+	{/if}
 {/each}
